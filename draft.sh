8b7306c498d5ca15e1009926249682f34517a385
VaryemezAmca <coder@kenardapara.com>
2018-05-06 22:02:58 +0200

    rebuilding site Sun May  6 22:02:58 CEST 2018

---
+++ draft.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "\033[0;32mPublishing drafts...\033[0m"
+
+# Build the project.
+hugo -D -d docs -b 'http://draft.kenardapara.com'
+
+git config user.name "VaryemezAmca"
+git config user.email "coder@kenardapara.com"
+
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+
+# Push source and build repos.
+git push origin master
