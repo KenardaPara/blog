5bff8026ca751b8251c16d47861f9f14ff418f78
VaryemezAmca <coder@kenardapara.com>
2018-05-21 15:35:20 +0200

    rebuilding site Mon May 21 15:35:20 CEST 2018

--- draft.sh
+++ draft.sh
@@ -3,6 +3,7 @@
 echo -e "\033[0;32mPublishing drafts...\033[0m"
 
 # Build the project.
+export HUGO_GOOGLEANALYTICS="x"
 hugo -D -d docs -b 'http://draft.kenardapara.com'
 
 git config user.name "VaryemezAmca"
