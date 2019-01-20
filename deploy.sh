0c56f37122c8cb57af4003e30b78d2360b8974ba
VaryemezAmca <coder@kenardapara.com>
2019-01-20 13:11:22 +0100

    Add simulayon

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,15 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+#simulasyonu ekle
+mkdir public/simulasyon
+
+cp -rvf simulasyon/besdata public/simulasyon
+cp simulasyon/charts.js public/simulasyon
+cp simulasyon/index.html public/simulasyon
+cp simulasyon/papaparse.min.js public/simulasyon
+cp simulasyon/ui.js public/simulasyon
+
 # Go To Public folder
 cd public
 
